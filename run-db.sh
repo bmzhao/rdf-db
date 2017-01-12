54b37e9bd0738aeb009b5575447035645d6ce157
Brian Zhao <bzspartan@gmail.com>
2017-01-11 16:27:07 -0800

    works with volume mounted at /indexed

--- run-db.sh
+++ run-db.sh
@@ -6,13 +6,16 @@ if [[ ! -d /indexed ]]; then
     mkdir /indexed
 fi
 
+cd /indexed
+
 if [[ ! -e /indexed/finished.txt ]]; then
     wget $INDEX_URL
     tar -xzvf indexed.tar.gz
+    mv /indexed/indexed/* /indexed
     touch /indexed/finished.txt
 fi
 
-#export JVM_ARGS=-Xmx4000M
+# export JVM_ARGS=-Xmx4000M
 
 cd /jena-fuseki
 ./fuseki-server --port=3030 --loc /indexed /custom
